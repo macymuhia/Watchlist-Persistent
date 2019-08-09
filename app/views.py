371eec9f168c915a1dcf39cbf4c14d743b1f8f59
macymuhia <macymuhia@gmail.com>
2019-08-09 12:30:08 +0300

    Add functions and routes for what will be displayed in the browser

--- app/views.py
+++ app/views.py
@@ -2,12 +2,27 @@ from flask import render_template
 
 from app import app
 
+from .request import get_movies
+
 # views
 @app.route("/")
 def index():
+    """
+    View root page function that returns the index page and its data
+    """
+
+    # Getting popular movie
+
+    popular_movies = get_movies("popular")
+    now_showing_movies = get_movies("now_playing")
+    up_coming_movies = get_movies("upcoming")
+    print(popular_movies)
+
     message = "Hellooo Macy"
     title = "Home - Best Movie Review Website"
-    return render_template("index.html", text=message, title=title)
+    return render_template(
+        "index.html", text=message, title=title, popular=popular_movies, now_showing=now_showing_movies, up_coming=up_coming_movies
+    )
 
 
 @app.route("/movie/<movie_id>")

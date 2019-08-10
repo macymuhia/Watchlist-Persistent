80e3a6539f2d526dfe1a1d2e0023785d7233dc4f
macymuhia <macymuhia@gmail.com>
2019-08-10 14:52:58 +0300

    Add a reviews class to handle reviews

--- app/models/movie.py
+++ app/models/movie.py
@@ -6,3 +6,21 @@ class Movie:
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
         self.rating = rating
         self.vote_count = vote_count
+
+
+class Review:
+    all_reviews = []
+
+    def __init__(self, movie_id, title, image_url, review):
+        self.movie_id = movie_id
+        self.title = title
+        self.image_url = image_url
+        self.review = review
+
+    def save_review(self):
+        Review.all_reviews.append(self)
+
+    @classmethod
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
+

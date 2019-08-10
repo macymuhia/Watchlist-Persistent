5ac181b8f6a96c80defc3b3e245a67b27023cf53
macymuhia <macymuhia@gmail.com>
2019-08-10 17:01:30 +0300

    Add a new review function and route to handle display of reviews logic

--- app/views.py
+++ app/views.py
@@ -4,6 +4,12 @@ from app import app
 
 from .request import get_movies, get_movie, search_movie
 
+from .models import reviews
+
+from .forms import ReviewForm
+
+Review = reviews.Review
+
 # views
 @app.route("/")
 def index():
@@ -32,9 +38,13 @@ def index():
 
 @app.route("/movie/<int:id>")
 def movie(id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
     movie = get_movie(id)
     title = f'{movie.title}'
-    return render_template("movie.html", title=title, movie=movie)
+    reviews = Review.get_reviews(movie.id)
+    return render_template("movie.html", title=title, movie=movie, reviews=reviews)
 
 
 @app.route('/search/<movie_name>')
@@ -47,3 +57,20 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'Search results for {movie_name}:'
     return render_template('search.html',movies=searched_movies, title=title)
+
+@app.route('/movie/review/new/<int:id>', methods=['GET', 'POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id, title, movie.poster, review)
+
+        new_review.save_review()
+        
+        return redirect(url_for('movie', id=id))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html', title=title, review_form=form, movie=movie)

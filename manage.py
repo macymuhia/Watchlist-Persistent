14e9a98cfe33fc775dd11e0fe660a55342d5fbc4
macymuhia <macymuhia@gmail.com>
2019-08-11 16:46:35 +0300

    Add flask migrate extension to create database migrations and Alembic will automatically create and track db migrations from changes to our db schema

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 from flask_script import Manager, Server
 from app import create_app, db
 from app.models import User, Role
+from flask_migrate import Migrate, MigrateCommand
 
 
 # Creating app instance
 app = create_app('development')
+migrate = Migrate(app,db)
 
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server',Server,)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():

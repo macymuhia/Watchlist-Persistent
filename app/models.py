87f37bab6ae202893c6a4bd222a0c3b9c56f4043
macymuhia <macymuhia@gmail.com>
2019-08-11 14:05:06 +0300

    Add a User class that will enable us create new users

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from . import db
+
+
 class Movie:
     def __init__(self, id, title, overview, poster, rating, vote_count):
         self.id = id
@@ -35,3 +38,14 @@ class Review:
                 response.append(review)
 
         return response
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+

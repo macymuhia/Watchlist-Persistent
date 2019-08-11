6e3d9f5f91a6b94a17ebd25531fda8eb70f99a38
macymuhia <macymuhia@gmail.com>
2019-08-11 15:28:46 +0300

    Add aforeignKey to the User class and create a role class with virtual column that will connect with the foreign key

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager, Server
 from app import create_app, db
-from app.models import User
+from app.models import User, Role
 
 
 # Creating app instance
@@ -11,7 +11,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Role=Role)
 
 if __name__ == "__main__":
     manager.run()
